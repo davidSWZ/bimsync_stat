bf5e050dae1e60e8e588f439477c7b7ed76f2e20
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2019-01-17 13:15:25 +0100

    Oauth complete, refresh works

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const express   = require("express"),
       mongoose  = require("mongoose"),
       oauths    = require("./models/oauth"),
       user      = require("./models/users"),
-      session   = require('express-session');
-
+      session   = require('express-session'),
+      bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(session({
   secret: 'bimsync secret',
   resave: false,
@@ -35,25 +36,24 @@ app.get("/oauth/redirect", function(req, res){
               "Content-Type": "application/x-www-form-urlencoded",
              }
   };
-
   function callback (err, response, body){
 //IF NO ERROR GET THE TOKEN
     if (!err && response.statusCode == 200) {
       var result = JSON.parse(body);
       var access_token = result.access_token;
 //GET THE IDENTITY OF THE CURRENT USER WITH THE TOKEN
-        var oauth = "Bearer " + access_token;
-        var options = {
+      var oauth = "Bearer " + access_token;
+      var options = {
           url:"https://api.bimsync.com/v2/user",
           headers:{
             Authorization: oauth
           }
-        }
-        request.get(options, function(err, response, body){
+        };
+      request.get(options, function(err, response, body){
           if(!err){
             var resultUser = JSON.parse(body);
 // CHECK IF THE USER ALREADY EXISTS
-            user.find({username:resultUser.username}).exec(function (err, currentUser){
+            user.find({username:resultUser.username}).populate("oauth").exec(function (err, currentUser){
 //IF NOT, SAVE THIS NEW USER
               if(!currentUser.length){
                 user.create(
@@ -63,44 +63,69 @@ app.get("/oauth/redirect", function(req, res){
                   }, function (err, createdUser) {
                       if (err) {
                         return handleError(err);
-                      }else{
+                      } else {
 //THEN SAVE THE OAUTH
-                      oauths.create(
+                        oauths.create(
                           { access_token: result.access_token,
-                          refresh_token: result.refresh_token,
-                          token_type: result.token_type,
-                          expires_in: result.expires_in
-                        }, function (err, createdOauth) {
+                            refresh_token: result.refresh_token,
+                            token_type: result.token_type,
+                            expires_in: result.expires_in
+                          }, function (err, createdOauth) {
                             if (err) {
                               return handleError(err);
-                            }else{
 //THEN ASSOCIATE THIS OAUTH TO THE USER
+                            } else {
                               user.findOneAndUpdate({username:resultUser.username},{oauth:createdOauth}, function(err, foundUser){
                                 if(err){
                                   console.log(err);
-                                }else{
+                                } else {
+                                  console.log("The user obtains a oauth");
                                   session.auth = createdOauth.access_token;
                                   console.log(session);
                                 }
                               })
                             }
-                           });
+                          });
                       }
-                     });
-              }else{
-                console.log("The User already exists");
-              }
-            })
-          }
-        });
+                     }
+                );
+              } else {
+//IF THE USER ALREADY EXISTS
+                console.log("The User already exists, his oauth is: "+ currentUser[0].oauth);
+                var optionsRefresh = {
+                  url: "https://api.bimsync.com/oauth2/token?grant_type=refresh_token&refresh_token="+currentUser[0].oauth.refresh_token+"&client_id="+clientID+"&client_secret="+client_Secret+"&redirect_uri=http://localhost:3000/oauth/redirect",
+                  headers: {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                           }
+                };
+                function callbackRefresh (err, response, bodyRefresh){
+                  var resultRefresh = JSON.parse(bodyRefresh);
+                  if (!err && response.statusCode == 200) {
+                    oauths.findByIdAndUpdate(
+                        currentUser[0].oauth._id,
+                        {
+                         access_token: resultRefresh.access_token,
+                         refresh_token: resultRefresh.refresh_token
+                       }, {new:true},function (err, updatedOauth){
+                          console.log("Access token mis à jour...");
+                          session.auth = updatedOauth.access_token;
+                     })
+                  } else {
+                    console.log(err);
+                  };
 
-//THEN REDIRECT TO THE HOME PAGE
+                };
+                request.post(optionsRefresh, callbackRefresh);
+              }
+            });
+          }
+
+      });
       res.redirect("/");
-    }else{
+    } else {
       console.log(err);
     }
   };
-
 // LAUNCH THE REQUEST
   request.post(options, callback);
 });
@@ -116,7 +141,8 @@ app.get("/projects", function (req, res){
   }
   request.get(options, function(err, response, body){
     if(!err){
-      res.send(body);
+      var result = JSON.parse(body)
+      res.render("projects", {result:result});
     }
     else{
       console.log(err);
@@ -124,6 +150,9 @@ app.get("/projects", function (req, res){
   })
 });
 
+app.post("/projects/models", function(req, res){
+  res.send(req.body);
+});
 
 app.listen(3000, process.env.IP, function(){
   console.log("the app run...");
