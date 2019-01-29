49621e5fd3cf1fd723f69a3e818ac81e4955a87d
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2019-01-29 13:26:04 +0100

    add cdn bootstrap

--- app.js
+++ app.js
@@ -202,6 +202,6 @@ app.post("/projects/models", function(req, res){
   res.send(req.body);
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
   console.log("the app run...");
 });
