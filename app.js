26249e8427b4eab386f0f223ea69b146e95a5e2d
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2019-01-30 21:45:27 +0100

    modified app use public for heroku

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const express   = require("express"),
       bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use("/public", express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(session({
   secret: 'bimsync secret',
